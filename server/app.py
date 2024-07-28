114778488d09486f5cd96debef8b22346fd51c7d
gamze <gamzekandara@gmail.com>
2024-07-28 16:13:00 -0400

    Adds app.py

--- server/app.py
+++ server/app.py
@@ -7,14 +7,51 @@ from flask_migrate import Migrate
 from models import db, Pet
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' # points to existing database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # set to Flase to avoid building up too much data in memeory when app is run.
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db) # app and db configured for Flask-Migrate.
 
-db.init_app(app)
+db.init_app(app) # db and app connects.
 
 # add views here 
+
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet directory!</h1>',
+        200
+    )
+    return response
+
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        response_body = f'<p>{pet.name} {pet.species}</p>'
+        response_status = 200
+    else: # error handling => 404 status code for Not Found
+        response_body = f'<p>Pet {id} not found</p>'
+        response_status = 404
+
+    response = make_response(response_body, response_status)
+    return response
+
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+
+    size = len(pets)  # all() returns a list so we can get length
+    response_body = f'<h2>There are {size} {species}s</h2>'
+    for pet in pets:
+        response_body += f'<p>{pet.name}</p>'
+    response = make_response(response_body, 200)
+    return response
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
